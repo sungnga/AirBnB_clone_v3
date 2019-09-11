6c7b0335d78e3826144990f43a21c9a485e54f66
Farrukh <679@holbertonschool.com>
2019-09-11 16:15:08 +0000

    Remove strict_slashes=False from some methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,7 +16,7 @@ def get():
     return jsonify(l)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'])
 def get_by_id(state_id):
     """Retrieve a state object by id"""
     obj = storage.get("State", state_id)
@@ -25,7 +25,7 @@ def get_by_id(state_id):
     return jsonify(obj.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'])
 def delete(state_id):
     """Delete a state object by id"""
     obj = storage.get("State", state_id)
@@ -50,7 +50,7 @@ def create():
     return jsonify(obj.to_dict()), 201
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['PUT'])
 def update(state_id):
     """Updates a state object"""
     if not request.get_json():
