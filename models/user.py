bf87af86a0417322f73b2f00eb6d99b758c8a6a8
sungnga <708@holbertonschool.com>
2019-09-12 21:40:15 +0000

    Add secure password method

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import hashlib
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,16 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        self.secure_pwd(self.password)
+
+
+    def __setattr__(self, k, v):
+        """sets user's password"""
+        if k == "password":
+            v = self.hashlib.md5(v.encode()).hexdigest()
+        super().__setattr__(k, v)
+
+
+    def secure_pwd(self, password):
+        """hashes user's password"""
+        self.password = hashlib.md5(v.encode()).hexdigest()
