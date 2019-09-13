58c712eb1f4b52b671d159480f9e41aaf3d1ac33
sungnga <708@holbertonschool.com>
2019-09-13 04:58:11 +0000

    Initial commit

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -78,3 +78,32 @@ def update_palce(place_id):
             setattr(obj, k, v)
     storage.save()
     return jsonify(obj.to_dict())
+
+
+@app_views.route('/places_search', methods=['POST'],
+                 strict_slashes=False)
+def search_places():
+    """Search a place object"""
+    dic = request.get_json()
+    if dic is None:
+        return jsonify({'error': 'Not a JSON'}), 400
+    res = []
+    places = storage.all('Place').values()
+    if not dic or not all(dic.values()):
+        res = [obj.to_dict() for obj in places]
+    elif 'amenities' in dic and dic['amenities']:
+        amenities = []
+        for place in places:
+            amenities += place.amenities
+        res = [obj.to_dict() for obj in amenities
+               if obj.id in dic['amenities']]
+    elif ('states' in dic and dic['states']) or ('cities' in
+                                                 dic and dic['cities']):
+        cities = []
+        states = [storage.get('State', i) for i in dic.get('states')]
+        for state in states:
+            cities += getattr(state, 'cities')
+        cities += [storage.get('City', i) for i in dic.get('cities')]
+        city_ids = [city.id for city in cities]
+        res = [place.to_dict() for place in places if place.id in city_ids]
+    return jsonify(res)
